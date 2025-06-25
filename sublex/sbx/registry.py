16b913958da0578bfdbeac4c3226404b490721e2
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-06-25 08:27:37 -0500

    Registry to hold loaded SBX files

---
+++ sublex/sbx/registry.py
@@ -0,0 +1,14 @@
+_sbx_registry = {}
+
+def register_sbx(filename, sbx_data):
+    _sbx_registry[filename] = sbx_data
+
+def get_all():
+    return list(_sbx_registry.values())
+
+def get_by_name(name):
+    for sbx in _sbx_registry.values():
+        if sbx.get('name') == name:
+            return sbx
+    return None
+
